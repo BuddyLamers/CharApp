3c1f31015722eff0d5d3caf4b15dad05bad6f1d9
App Academy Student <student@appacademy.io>
2014-04-21 17:39:15 -0400

    creates directory structure

---
+++ CharApp/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CharApp::Application.config.secret_key_base = '47608dce78f0d537a25ed2bac006adfb0e4804eb5f18a29d2ed4617314310d6e2b4b031bc313fd5fb15bf07838d9ee3e54c67752604357b87668c0d732851e2b'
